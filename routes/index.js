06598686fb8925a500773644de0a905828a4f901
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-04-06 21:47:15 -0500

    database connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models").User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { fat: 'LOL POOP' });
+});
+
+router.get('/user', function(req, res, next) {
+    res.render('user', { user: {name: "pooper", pass: "12345"} });
+});
+
+router.post('/user', function(req, res, next) {
+  User.create(req.body).then(function (user) {
+      res.redirect('user');
+  });
 });
 
 module.exports = router;
